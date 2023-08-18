086b6519f2fccb45e1113b1d37065b1565d5bb86
saman <itsawmi@gmail.com>
2023-08-19 02:50:36 +0330

    added

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import random
 
 random_number = random.randint(0,100)
-user_numbr = int(input("hii, my developer is SamanRezagholi , please send me a number between 0 to 100:"))
+user_number = int(input("hii, my developer is SamanRezagholi , please send me a number between 0 to 100:"))
 
-while user_numbr != random_number :
+while user_number != random_number :
 # if userNumberInput not equal with randNumber :
     if user_number > random_number :
-        print (f"nooo :( , {user_numbr} is big")
+        print (f"nooo :( , {user_number} is big")
         user_number = int(input("please send me a number:"))
 
     else : 
-        print (f"nooo :( , {user_numbr} is small")
+        print (f"nooo :( , {user_number} is small")
         user_number = int(input("please send me a number:"))
 
-print (f"good, correctNumber is {random_number} and your answer is {user_numbr}")                  
+print (f"good, correctNumber is {random_number} and your answer is {user_number}")                  
